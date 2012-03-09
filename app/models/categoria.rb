3e096b7f0d932295d4fe9697ee505871e3c03831
domthu <dom_thual@yahoo.fr>
2012-03-09 15:11:54 +0100

    categorie per sezione

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -1,4 +1,7 @@
 class Categoria < ActiveRecord::Base
 belongs_to :sezione
 
+#named_scope :categories_per_sezione, lambda {|ids| {:conditions => {:sezioni_id => ids}}}
+
+
 end
--- app/models/sezione.rb
+++ app/models/sezione.rb
@@ -1,5 +1,12 @@
 class Sezione < ActiveRecord::Base
 has_many :categorie
 
-named_scope :for_categorie_ids, lambda {|ids| {:conditions => {:categoria_id => ids}}}
+
+    def get_categorie
+        #@fs = FilterSample.all(:conditions => ['sampling_id = ?', @sampling.id ])
+        Categoria.where(["sezioni_id >= ?", id.to_s])
+        #Categoria.all()
+    end
+
+
 end
